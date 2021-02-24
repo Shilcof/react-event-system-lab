8bd00f4d6389cb555e8ba1720e855f302cff4b65
Shilcof <robert.shilcof@gmail.com>
2021-02-24 13:56:37 +0000

    commit tree bdee77b5f0559247c2620785634711d5f9195d37

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+export default class EyesOnMe extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleOnFocus(event) {
+        console.log("Good!");
+    }
+
+    handleOnBlur(event) {
+        console.log("Hey! Eyes on me!");
+    }
+
+    render() {
+        return (
+            <button
+                onFocus={this.handleOnFocus}
+                onBlur={this.handleOnBlur}
+            ></button>
+        )
+    }
+
+}
